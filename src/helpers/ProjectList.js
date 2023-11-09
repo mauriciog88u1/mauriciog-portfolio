ea69fc7837e52926dbfbf2b54bac2eebeefbc54e
mauriciog88u1 <83040257+mauriciog88u1@users.noreply.github.com>
2023-11-09 15:58:21 -0700

    ui changes

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,22 @@
+import proj1 from "../static/images/mauricioPort.jpg";
+
+export const ProjectList = [
+    {
+        name: "Personal Portfolio Website",
+        image: proj1,
+        id: 1,
+        description: "A personal portfolio website to showcase my projects and skills.",
+        link: "#",
+        tools: "React, Material-UI",
+        skills: "React, Material-UI, HTML, CSS, JavaScript, Git, GitHub, CloudFlare"
+    },
+    {
+        name: "Web Based Map Tool",
+        image: proj1,
+        id: 2,
+        description: "A web based map tool to help users find the the most optimal route given a set of locations. Using algorithms such as Nearest Neighbor and 2-Opt.",
+        link: "#",
+        tools: "Java Spark, React",
+        skills: "Technologies: IntelliJ IDEA, Linux, Docker, Git, GitHub, Maven, NPM, Webpack, JUnit, Jest, Postman, Zenhub, and Code Climate. The technology tools used include: Linux, Java, JavaScript, React, Reactstrap, React Leaflet, SQL, MariaDB, Java Spark, Concurrency, SLF4J, uLog, JSON, CSV, SVG, and KML"
+    }
+];
--- src/pages/ProjectDisplay.js
+++ src/pages/ProjectDisplay.js
@@ -5,18 +5,38 @@ import GitHubIcon from "@material-ui/icons/GitHub";
 import "../styles/ProjectDisplay.css";
 
 function ProjectDisplay() {
-  const { id } = useParams();
-  const project = ProjectList[id];
-  return (
-    <div className="project">
-      <h1> {project.name}</h1>
-      <img src={project.image} />
-      <p>
-        <b>Skills:</b> {project.skills}
-      </p>
-      <GitHubIcon />
-    </div>
-  );
+    const { id } = useParams();
+    const project = ProjectList[id];
+
+    // Function to render skills with a maximum of five per line
+    const renderSkills = (skills) => {
+        return skills.split(', ').reduce((acc, skill, index) => {
+            const line = Math.floor(index / 5);
+            if (!acc[line]) {
+                acc[line] = [];
+            }
+            acc[line].push(skill);
+            return acc;
+        }, []).map((skillLine, index) => (
+            <div key={index} className="skill-line">
+                {skillLine.join(', ')}
+            </div>
+        ));
+    };
+
+    return (
+        <div className="project">
+            <h1> {project.name}</h1>
+            <img src={project.image} alt={project.name} />
+            <div className="skills">
+                <b>Skills:</b>
+                <div className="skills-list">
+                    {renderSkills(project.skills)}
+                </div>
+            </div>
+            <GitHubIcon />
+        </div>
+    );
 }
 
 export default ProjectDisplay;
